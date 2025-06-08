22bd8c46087c2e409d7635572240c9edf644ae1e
Eduardo Klitzke Oliveira <EduardoKlitzkee@gmail.com>
2025-06-07 21:59:17 -0300

    Implementando a injeção de dependência entre as classes Controller e Service

--- src/main/java/com/klitzkee/CursoSpring/controller/Controller.java
+++ src/main/java/com/klitzkee/CursoSpring/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.klitzkee.CursoSpring.controller;
 
+import com.klitzkee.CursoSpring.service.Services;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/controller")  //http://localhost:8080/controller
 public class Controller {
 
+    //Atributos
+
+    //Associações
+    @Autowired
+    private Services services;
+
     //Metodo http que ele recebe, pode set GET, POST, DELETE, PUT, etc...
+    //Aqui definimos que esse metodo vai se conectar com a classe de services
     @GetMapping
     public String helloController() {
-        return "Hello";
+        return services.helloService("Eduardo");
     }
     
 }
--- src/main/java/com/klitzkee/CursoSpring/service/Services.java
+++ src/main/java/com/klitzkee/CursoSpring/service/Services.java
@@ -1,4 +1,14 @@
 package com.klitzkee.CursoSpring.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Service
 public class Services {
+
+    //Metodo http que ele recebe, pode set GET, POST, DELETE, PUT, etc...
+    @GetMapping
+    public String helloService(String name) {
+        return "Hello service!" + name;
+    }
 }
